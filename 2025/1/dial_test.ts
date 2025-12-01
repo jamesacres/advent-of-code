4e998b39646bf16a18f0965bf0b3697892f8d429
James Acres <jamesacres@gmail.com>
2025-12-01 20:33:53 +0000

    day 1 part 1 input

--- 2025/1/dial_test.ts
+++ 2025/1/dial_test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "@std/assert";
 import { numberOfZeros, Rotation } from "./dial.ts";
 
-Deno.test(function testInputLandOnZero() {
+Deno.test(function exampleInputLandOnZero() {
   const input = `L68
 L30
 R48
@@ -21,5 +21,19 @@ L82`;
   assertEquals(
     numberOfZeros(50, 99, rotations),
     3,
+  );
+});
+
+Deno.test(async function testInputLandOnZero() {
+  const input = await Deno.readTextFile("./2025/1/input.txt");
+  const rotations: Rotation[] = input.split("\n").map((line) => {
+    return {
+      direction: line.charAt(0) as "L" | "R",
+      distance: Number(line.slice(1)),
+    };
+  });
+  assertEquals(
+    numberOfZeros(50, 99, rotations),
+    1092,
   );
 });
