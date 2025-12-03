9fdfa9a2615d0fa941bc488b6fe583a292cc7f8f
James Acres <jamesacres@gmail.com>
2025-12-03 21:46:05 +0000

    day 3 part 2

--- 2025/3/joltage.ts
+++ 2025/3/joltage.ts
@@ -1,10 +1,19 @@
-export const maxJoltage = (bank: string): number => {
-  const batteries = bank.split("").map(Number);
-  const combinations: number[] = [];
-  for (const [i, battery] of batteries.entries()) {
-    for (let j = i + 1; j < batteries.length; j = j + 1) {
-      combinations.push(parseInt(`${battery}${batteries[j]}`));
-    }
+export const maxJoltage = (bank: string, count: number): number => {
+  const batteryEntries = bank.split("").map((value, i) => [i, Number(value)]);
+  const orderedEntries = batteryEntries.sort(([_a, a], [_b, b]) => b - a);
+
+  const getNextHighestWithRemainder = (minIndex: number, maxIndex: number) =>
+    orderedEntries.find(([j]) => j > minIndex && j < maxIndex);
+
+  let currentIndex = -1;
+  let currentValue = "";
+  for (let n = 0; n < count; n++) {
+    const [nextIndex, nextValue] = getNextHighestWithRemainder(
+      currentIndex,
+      batteryEntries.length - count + n + 1,
+    )!;
+    currentValue = `${currentValue}${nextValue}`;
+    currentIndex = nextIndex;
   }
-  return combinations.sort()[combinations.length - 1];
+  return Number(currentValue);
 };
--- 2025/3/joltage_test.ts
+++ 2025/3/joltage_test.ts
@@ -9,7 +9,7 @@ const exampleInput = `987654321111111
 for (const [i, expected] of [98, 89, 78, 92].entries()) {
   Deno.test(function maxJoltagePerBank() {
     assertEquals(
-      maxJoltage(exampleInput.split("\n")[i]),
+      maxJoltage(exampleInput.split("\n")[i], 2),
       expected,
     );
   });
@@ -17,7 +17,10 @@ for (const [i, expected] of [98, 89, 78, 92].entries()) {
 
 Deno.test(function exampleSumMaxJoltage() {
   assertEquals(
-    exampleInput.split("\n").map(maxJoltage).reduce((a, b) => a + b),
+    exampleInput.split("\n").map((input) => maxJoltage(input, 2)).reduce((
+      a,
+      b,
+    ) => a + b),
     357,
   );
 });
@@ -25,7 +28,35 @@ Deno.test(function exampleSumMaxJoltage() {
 Deno.test(async function inputSumMaxJoltage() {
   const input = await Deno.readTextFile("./2025/3/input.txt");
   assertEquals(
-    input.split("\n").map(maxJoltage).reduce((a, b) => a + b),
+    input.split("\n").map((input) => maxJoltage(input, 2)).reduce((a, b) =>
+      a + b
+    ),
     17432,
+  );
+});
+
+for (
+  const [i, expected] of [
+    987654321111,
+    811111111119,
+    434234234278,
+    888911112111,
+  ].entries()
+) {
+  Deno.test(function maxJoltagePerBankTwelve() {
+    assertEquals(
+      maxJoltage(exampleInput.split("\n")[i], 12),
+      expected,
+    );
+  });
+}
+
+Deno.test(async function inputSumMaxJoltage() {
+  const input = await Deno.readTextFile("./2025/3/input.txt");
+  assertEquals(
+    input.split("\n").map((input) => maxJoltage(input, 12)).reduce((a, b) =>
+      a + b
+    ),
+    173065202451341,
   );
 });
