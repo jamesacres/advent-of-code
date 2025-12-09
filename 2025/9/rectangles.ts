7230eef7e6c534f0a4ea0668a40717ee46015a51
James Acres <jamesacres@gmail.com>
2025-12-09 22:13:55 +0000

    day 9 part 2

--- 2025/9/rectangles.ts
+++ 2025/9/rectangles.ts
@@ -15,3 +15,23 @@ export const findRectangles = (corners: string[]) => {
     bArea - aArea
   );
 };
+
+export const findRectangles2 = (corner1: string, corners: string[]) => {
+  const rectangles: [string, string, number][] = [];
+  const [x1, y1] = corner1.split(",").map(Number);
+  for (const corner2 of corners) {
+    const [x2, y2] = corner2.split(",").map(Number);
+    if (x1 === x2 || y1 === y2) {
+      continue;
+    }
+    // must be less than this as that's the curve of circle
+    if (y2 > 68076) {
+      continue;
+    }
+    const area = (Math.abs(x2 - x1) + 1) * (Math.abs(y2 - y1) + 1);
+    rectangles.push([corner1, corner2, area]);
+  }
+  return rectangles.sort(([_a1, _a2, aArea], [_b1, _b2, bArea]) =>
+    bArea - aArea
+  );
+};
--- 2025/9/rectangles_test.ts
+++ 2025/9/rectangles_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert/equals";
-import { findRectangles } from "./rectangles.ts";
+import { findRectangles, findRectangles2 } from "./rectangles.ts";
 
 const exampleInput = `7,1
 11,1
@@ -22,5 +22,15 @@ Deno.test(async function inputLargestRectangle() {
   assertEquals(
     findRectangles(input.split("\n"))[0][2],
     4743645488,
+  );
+});
+
+Deno.test(async function inputLargestRectangle2() {
+  // guess based on svg
+  // assertEquals((Math.abs(x2 - x1) + 1) * (Math.abs(y2 - y1) + 1), 1525973456);
+  const input = await Deno.readTextFile("./2025/9/inputsubset.txt");
+  assertEquals(
+    findRectangles2("94699,50401", input.split("\n")).slice(0, 10)[0][2],
+    1529011204,
   );
 });
